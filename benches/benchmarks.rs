aba868e0b519c25688d093d8fb922674e9844f2d
Alexander Weiss <alex@weissfam.de>
2025-07-18 13:16:53 +0200

    modify benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -6,11 +6,11 @@ use rustic_cdc::{Rabin64, RollingHash64};
 /// Benchmark the sliding window of the Rabin64 algorithm
 ///
 pub fn slide_benchmarks(c: &mut Criterion) {
-    for i in [1_000, 10_000, 100_000] {
+    for i in [100_000] {
         _ = c.bench_function(&format!("slide {i}x"), |b| {
             let data: u8 = 16; //arbitrary value
+            let mut rabin = Rabin64::new(5);
             b.iter(|| {
-                let mut rabin = Rabin64::new(5);
                 for _ in 0..i {
                     rabin.slide(data);
                 }
